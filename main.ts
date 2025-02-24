c5fea71852789b3879f492cd2b067cbb2acda327
Kris Zebarth <kris.zebarth@ocsb.ca>
2025-02-23 21:56:12 -0500

    Added radio functions

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("Hello")
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString("Message:" + receivedString)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("Goodbye")
 })
